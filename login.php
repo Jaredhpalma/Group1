234074492d377d2046a3859fc675bf8f83e707e8
Jaredhpalma <sormelajared@gmail.com>
2025-03-14 14:57:32 +0800

    updated login and register

--- login.php
+++ login.php
@@ -51,7 +51,7 @@
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                   <img src="assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block">NiceAdmin</span>
+                  <span class="d-none d-lg-block">MIMS</span>
                 </a>
               </div><!-- End Logo -->
 
@@ -87,11 +87,11 @@
                         <label class="form-check-label" for="rememberMe">Remember me</label>
                       </div>
                     </div>
-                    <div class="col-12">
-                      <button class="btn btn-primary w-100" type="submit">Login</button>
+                    <div class="col-12"><a href="index.php"></a>
+                      <button class="btn btn-primary w-100" type="submit"><a href="index.php">Login</a></button>
                     </div>
                     <div class="col-12">
-                      <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
+                      <p class="small mb-0">Don't have account? <a href="register.php">Create an account</a></p>
                     </div>
                   </form>
 
